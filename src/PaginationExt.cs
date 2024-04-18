581684dfcb8342f7c1307d4cd82cbed0343284bc
DarkCompet <darkcompet@gmail.com>
2024-04-18 14:06:03 +0700

    Introduce pager class

--- src/PaginationExt.cs
+++ src/PaginationExt.cs
@@ -1,99 +1,107 @@
 namespace Tool.Compet.EntityFrameworkCore;
 
+using System.Text.Json.Serialization;
 using Microsoft.EntityFrameworkCore;
 
-// Extension of IQueryable<T>
-// Where T: item type
+/// <summary>
+/// Extension for IQueryable<T>, where T is item type.
+/// </summary>
 public static class PaginationExt {
-	/// Note: given `pageIndex * pageSize` must be in range of Int32.
-	/// @param pagePos: Index (from 1) of the page. For eg,. 1, 2, 3,...
-	/// @param pageSize: Item count in the page. For eg,. 10, 20, 50,...
-	public static async Task<PagedResult<T>> PaginateDk<T>(
+	/// <summary>
+	/// Note: given `page * item` should be in range of Int32.
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	/// <param name="query"></param>
+	/// <param name="page">Page number (index 1-based)</param>
+	/// <param name="limit">Item count in the page. For eg,. 10, 20, 50,...</param>
+	/// <returns></returns>
+	public static async Task<PagedResult<T>> PaginateAsyncDk<T>(
 		this IQueryable<T> query,
-		int pagePos,
-		int pageSize
+		int page,
+		int limit
 	) where T : class {
 		// Offset equals to item-count as far to now. For eg,. offset is 50 when query at page 2 with item 50.
 		// TechNote: use max to prevent negative index from overflow
-		var offset = Math.Max(0, (pagePos - 1) * pageSize);
+		var offset = Math.Max(0, (page - 1) * limit);
 
 		// Number of all items of the query.
 		var totalItemCount = await query.CountAsync();
 
-		// Query and take some items in range [offset, offset + pageSize - 1]
-		var items = await query.Skip(offset).Take(pageSize).ToArrayAsync();
+		// Query and take some items in range [offset, offset + limit - 1]
+		var items = await query.Skip(offset).Take(limit).ToArrayAsync();
 
 		// Number of page.
-		// This calculation is faster than `Math.Ceiling(rowCount / pageSize)`
-		var pageCount = (totalItemCount + pageSize - 1) / pageSize;
+		// This calculation is faster than `Math.Ceiling(rowCount / limit)`
+		var pageCount = Math.Max(0, totalItemCount + limit - 1) / limit;
 
 		return new PagedResult<T>(
 			items: items,
-			pagePos: pagePos,
-			pageCount: pageCount,
-			totalItemCount: totalItemCount
+			pager: new(page, pageCount, totalItemCount)
 		);
 	}
 
-	/// Note: given `pageIndex * pageSize` must be in range of Int32.
-	/// @param leftPaddingItems: Items which be added at left of query result.
-	/// @param pagePos: Index (from 1) of the page. For eg,. 1, 2, 3,...
-	/// @param pageSize: Item count in the page. For eg,. 10, 20, 50,...
-	public static async Task<PagedResult<T>> PaginateDk<T>(
+	/// <summary>
+	/// Note: given `page * item` must be in range of Int32.
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	/// <param name="query"></param>
+	/// <param name="leftPaddingItems">Items which be added at left of query result.</param>
+	/// <param name="page">Page number (index 1-based)</param>
+	/// <param name="limit">Item count in the page. For eg,. 10, 20, 50,...</param>
+	/// <returns></returns>
+	public static async Task<PagedResult<T>> PaginateAsyncDk<T>(
 		this IQueryable<T> query,
 		IEnumerable<T> leftPaddingItems,
-		int pagePos,
-		int pageSize
+		int page,
+		int limit
 	) where T : class {
 		// Offset equals to item-count as far to now. For eg,. offset is 50 when query at page 2 with item 50.
 		// TechNote: use max to prevent negative index from overflow
-		var offset = Math.Max(0, (pagePos - 1) * pageSize);
+		var offset = Math.Max(0, (page - 1) * limit);
 
 		var leftPaddingItemCount = leftPaddingItems.Count();
 		var totalItemCount = leftPaddingItemCount + (await query.CountAsync());
 
-		// Query and take some items in range [offset, offset + pageSize - 1]
-		var items = new List<T>();
-		var takeCount1 = Math.Min(leftPaddingItemCount - offset, pageSize);
+		// Query and take some items in range [offset, offset + limit - 1]
+		var takeItems = new List<T>();
+		var takeCount1 = Math.Min(leftPaddingItemCount - offset, limit);
 		if (takeCount1 > 0) {
-			items.AddRange(leftPaddingItems.Skip(offset).Take(takeCount1));
+			takeItems.AddRange(leftPaddingItems.Skip(offset).Take(takeCount1));
 		}
-		var takeCount2 = pageSize - items.Count;
+		var takeCount2 = limit - takeItems.Count;
 		if (takeCount2 > 0) {
 			var skipCount = Math.Max(0, offset - leftPaddingItemCount);
-			items.AddRange(await query.Skip(skipCount).Take(takeCount2).ToArrayAsync());
+			takeItems.AddRange(await query.Skip(skipCount).Take(takeCount2).ToArrayAsync());
 		}
 
-		// This calculation is faster than `Math.Ceiling(rowCount / pageSize)`
-		var pageCount = (totalItemCount + pageSize - 1) / pageSize;
+		// This calculation is faster than `Math.Ceiling(rowCount / limit)`
+		var pageCount = Math.Max(0, totalItemCount + limit - 1) / limit;
 
 		return new PagedResult<T>(
-			items: items.ToArray(),
-			pagePos: pagePos,
-			pageCount: pageCount,
-			totalItemCount: totalItemCount
+			items: [.. takeItems],
+			pager: new(page, pageCount, totalItemCount)
 		);
 	}
 }
 
-public class PagedResult<T> where T : class {
+public class PagedResult<T>(T[] items, Pager pager) where T : class {
 	/// Items in the page.
 	/// Note: can use `IEnumerable<T>` for more abstract that can cover both of array and list.
-	public readonly T[] items;
+	public readonly T[] items = items;
 
-	/// Position (1-index-based) of current page
-	public readonly int pagePos;
+	public readonly Pager pager = pager;
+}
 
-	/// Total number of page
-	public readonly int pageCount;
+public class Pager(int page, int count, int total) {
+	/// Page position (1-based index)
+	[JsonPropertyName("page")]
+	public int page { get; set; } = page;
+
+	/// Page count
+	[JsonPropertyName("count")]
+	public int count { get; set; } = count;
 
 	/// Total item count
-	public readonly int totalItemCount;
-
-	public PagedResult(T[] items, int pagePos, int pageCount, int totalItemCount) {
-		this.items = items;
-		this.pagePos = pagePos;
-		this.pageCount = pageCount;
-		this.totalItemCount = totalItemCount;
-	}
+	[JsonPropertyName("total")]
+	public int total { get; set; } = total;
 }
